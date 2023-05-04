306d8e4a62b7c4f32b0749141637d4de3afe1f8c
Thebks <ichbks@gmail.com>
2023-05-04 19:48:28 +0500

    server

---
+++ server/index.js
@@ -0,0 +1,30 @@
+import express from "express"
+import bodyParser from "body-parser"
+import mongoose from "mongoose";
+import cors from "cors"
+import dotenv from "dotenv"
+import helmet from "helmet"
+import morgan from "morgan"
+
+// Middleware functions
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+console.log("Yes its Working")
+console.log("Check again")
+// ROUTES
+
+const PORT = process.env.PORT || 7000;
+mongoose.connect(process.env.MONGO_DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(async () => {
+        app.listen(PORT, () => console.log(`Server Port ${PORT}`))
+    })
+    .catch((error) => console.log(`${error} did not connect`))
